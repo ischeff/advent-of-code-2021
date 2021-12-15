8a246f2fbddccc664b5ad4c3e5e8b1d73b31869a
Ian Scheffler <ian.f.scheffler@gmail.com>
2021-12-14 21:34:55 -0500

    making progress, but not incrementing prevDepth correctly

--- sonar-sweep/sonar-sweep.py
+++ sonar-sweep/sonar-sweep.py
@@ -1,12 +1,22 @@
 import itertools
-with open('data.txt') as depths:
+with open('data.txt') as f:
+    depths = [int(i) for i in f]
     # data = depths.read()
     # print(data)
     # prevDepth = depths[0]
-    # counter = 0
+    counter = 0
     # currentDepth = 0
-    for line in itertools.islice(depths, 1, None):
-        print(line)
+    for currentDepth in itertools.islice(depths, 1, None):
+        print("Previous Depth is: " + str(prevDepth))
+        print("Current Depth is: " + str(currentDepth))
+        print("Is the current depth deeper than the previous depth?")
+        if currentDepth > prevDepth:
+            counter = counter + 1
+            print("Yes, the current depth is deeper.")
+        else:
+            print("No, the current depth is not deeper")
+        currentDepth = prevDepth
+    print("The depth has increased " + str(counter - 1) + " times.")
 
 
 # questions: since the list is 1000s of values long, what's the most efficient way to do this? Should I sort the list? Or wouldn't that defeat the whole purpose, since it represnts the ocean floor, and you can't order that?
