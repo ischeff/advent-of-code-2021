468d78271e55af5f250eb097b9d5790b0711e6ca
Ian Scheffler <ian.f.scheffler@gmail.com>
2021-12-14 22:17:45 -0500

    commented out tests

--- sonar-sweep/sonar-sweep.py
+++ sonar-sweep/sonar-sweep.py
@@ -4,14 +4,14 @@ with open('data.txt') as f:
     counter = 0
     prevDepth = depths[0]
     for currentDepth in itertools.islice(depths, 1, None):
-        print("Previous Depth is: " + str(prevDepth))
-        print("Current Depth is: " + str(currentDepth))
-        print("Is the current depth deeper than the previous depth?")
+        # print("Previous Depth is: " + str(prevDepth))
+        # print("Current Depth is: " + str(currentDepth))
+        # print("Is the current depth deeper than the previous depth?")
         if currentDepth > prevDepth:
             counter += 1
-            print("Yes, the current depth is deeper.")
-        else:
-            print("No, the current depth is not deeper")
-        print("The total number of times the depth has decreased is: " + str(counter))
+        #     print("Yes, the current depth is deeper.")
+        # else:
+        #     print("No, the current depth is not deeper")
+        # print("The total number of times the depth has decreased is: " + str(counter))
         prevDepth = currentDepth
     print("The depth has increased " + str(counter) + " times.")
