0a8dfe0fc121e336b8710f9b907594f2b19113eb
Ian Scheffler <ian.f.scheffler@gmail.com>
2021-12-31 13:56:45 -0500

    added finishing touches

--- sonar-sweep/sonar-sweep.py
+++ sonar-sweep/sonar-sweep.py
@@ -28,8 +28,12 @@ def sonar_sweep_part_one_zip(depths):
             counter += 1
     print(f"The depth has increased {counter} times.")
 
-# def sonar_sweep_part_two_zip(depths):
-
+def sonar_sweep_part_two_zip(depths):
+    counter = 0
+    for prevDepth, currentDepth in zip(depths, depths[3:]):
+        if currentDepth > prevDepth:
+            counter += 1
+    print(f"The sum of three consecutive depths has increased {counter} times.")
 
 #~~~~~~~~~~ solutions using list comprehension ~~~~~~~~~~~~~~~~~#
 
@@ -37,17 +41,26 @@ def sonar_sweep_part_one_comprehension(depths):
     depthIncreases = [1 for (left, right) in zip(depths, depths[1:]) if right > left]
     print(f"The depth has increased {len(depthIncreases)} times.")
 
-# def sonar_sweep_part_two_comprehension(depths):
+def sonar_sweep_part_two_comprehension(depths):
+    depthIncreases = [1 for (left, right) in zip(depths, depths[3:]) if right > left]
+    print(f"The sum of three consecutive depths has increased {len(depthIncreases)} times.")
 
 #~~~~~~~~~~~~~~~~solutions using sum/zip~~~~~~~~~~~~~~~~~~~~~~~~#
 
 def sonar_sweep_part_one_sum(depths):
-    return sum(1 for (left, right) in zip(depths, depths[1:]) if right > left)
+    depthIncreases = sum(1 for (left, right) in zip(depths, depths[1:]) if right > left)
+    print(f"The depth has increased {depthIncreases} times.")
 
+def sonar_sweep_part_two_sum(depths):
+    depthIncreases = sum(1 for (left, right) in zip(depths, depths[3:]) if right > left)
+    print(f"The sum of three consecutive depths has increased {depthIncreases} times.")
 
 if __name__ == "__main__":
     sonar_sweep_part_one(load_input('data.txt'))
     sonar_sweep_part_two(load_input('data.txt'))
     sonar_sweep_part_one_zip(load_input('data.txt'))
+    sonar_sweep_part_two_zip(load_input('data.txt'))
     sonar_sweep_part_one_comprehension(load_input('data.txt'))
-    print(sonar_sweep_part_one_sum(load_input('data.txt')))
+    sonar_sweep_part_two_comprehension(load_input('data.txt'))
+    sonar_sweep_part_one_sum(load_input('data.txt'))
+    sonar_sweep_part_two_sum(load_input('data.txt'))
