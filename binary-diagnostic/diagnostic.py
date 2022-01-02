74e4aa35a2bc94b660ad7985382151526be3e0be
Ian Scheffler <ian.f.scheffler@gmail.com>
2022-01-02 12:42:03 -0500

    trying new approach to part 2

--- binary-diagnostic/diagnostic.py
+++ binary-diagnostic/diagnostic.py
@@ -34,7 +34,34 @@ def part_one_b(sums, report):
 # try using a list comprehension to filter report -
 
 def part_two(sums, report):
-    # make two copies of report (one for oxygen and one for C02)
+    oxygen = []
+    c02 = []
+    for i in range(len(report)):
+        line = report[i]
+        for j in range(len(line)):
+            if sums[j] >= len(report)/2 and line[j] == "1":#1 is most common number so add 1s to oxygen
+                oxygen.append(line)
+            elif sums[j] < len(report)/2 and line[j] == "0": #0 is most common number so add 0s to oxygen
+                oxygen.append(line)
+            elif sums[j] >= len(report)/2 and line[j] == "0": #1 is most common number so add 0s to co2
+                c02.append(line)
+            elif sums[j] < len(report)/2 and line[j] == "1": #0 is most common number so add 1s to co2
+                c02.append(line)
+    return oxygen, co2
+
+
+# def part_two(sums, report):
+#     # make two copies of report (one for oxygen and one for C02)
+#     oxygen = [x for x in report]
+#     co2 = [x for x in report]
+#     for i in range(len(sums)):
+#         if sums[i] >= len(report)/2:
+#             oxygen = [x for x in oxygen if oxygen[i] != "1"]
+#             c02 = [x for x in co2 if co2[i] != "0"]
+#         else:
+#              oxygen = [x for x in oxygen if oxygen[i] != "0"]
+#              c02 = [x for x in co2 if co2[i] != "1"]
+#         return oxygen, c02
     #if the sum for a given position is > 500
         # then remove ANY item in oxygen list that does NOT have a 1 at that position
         # then remove ANY item in CO2 that does NOT have a 0 at that position
@@ -54,3 +81,4 @@ if __name__ == "__main__":
     # print(len(load_input("data.txt")))
     print(part_one_a(load_input("data.txt")))
     print(part_one_b(part_one_a(load_input("data.txt")), load_input("data.txt")))
+    print(part_two(part_one_a(load_input("data.txt")), load_input("data.txt")))
