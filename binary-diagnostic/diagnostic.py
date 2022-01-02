6f61adc1c9e6f4b1bddb649bd91bf0a9f62001bc
Ian Scheffler <ian.f.scheffler@gmail.com>
2022-01-02 11:12:47 -0500

    removed magic 500 in line 24

--- binary-diagnostic/diagnostic.py
+++ binary-diagnostic/diagnostic.py
@@ -16,12 +16,12 @@ def part_one_a(report):
 # since the len of the report is 1000 lines, if the sum is less than 500,
 # the majority of the bits in that column are 0s and vice versa
 # Note: I'm gambling that there is not an even number of 1s and 0s in each position
-# since the rules don't say anything about tiebreakers! 
-def part_one_b(sums):
+# since the rules don't say anything about tiebreakers!
+def part_one_b(sums, report):
     gamma = "" # most common bit
     epsilon = "" # least common bit
     for i in range(len(sums)):
-        if sums[i] > 500:
+        if sums[i] > len(report)/2:
             gamma += "1"
             epsilon += "0"
         else:
@@ -42,4 +42,4 @@ if __name__ == "__main__":
     # print(load_input("data.txt"))
     # print(len(load_input("data.txt")))
     print(part_one_a(load_input("data.txt")))
-    print(part_one_b(part_one_a(load_input("data.txt"))))
+    print(part_one_b(part_one_a(load_input("data.txt")), load_input("data.txt")))
