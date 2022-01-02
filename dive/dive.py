adc28f4542d331ff0b187a3a3d54a7e1e01fdf32
Ian Scheffler <ian.f.scheffler@gmail.com>
2022-01-01 23:48:23 -0500

    starting solution

--- dive/dive.py
+++ dive/dive.py
@@ -1,3 +1,19 @@
+def load_input(data):
+    with open(data) as f:
+        course = f.readlines()
+        return course
+
+#~~~~~~~~~~~~~~~~~~~~~ naive solutions ~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+def part_one(course):
+    x = 0
+    y = 0
+    for line in course:
+        direction, magnitude = course.split()
+        magnitude = int(magnitude)
+        if direction.startswith() == "f":
+
+
 # get the input
 # how should you process it?
 # input is [forward/up/down NUM]
@@ -11,4 +27,7 @@
     # if the line starts with "d", add number that follows to y coordinate (vertical)
     # else (if the line starts with "u"), subtract number that follows from y coordinate (vertical)
 
-# assign variable to product of final coordinates 
+# assign variable to product of final coordinates
+
+if __name__ == "__main__":
+    print(load_input("data.txt"))
